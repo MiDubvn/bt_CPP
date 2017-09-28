6f42c8ab9e2339222e92b22f894434ace1d88992
Nguyen Tuan Kiet <nguyentuankiet23031998@gmail.com>
2017-09-28 17:08:52 +0700

    Con trỏ hàm

--- tuan3/contro.cpp
+++ tuan3/contro.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<conio.h>
-#define MAX 100
+#define MAX 1000
 struct SV
 {
 	int MSSV;
@@ -9,43 +9,58 @@ struct SV
 	float ly;
 	float hoa;
 };
-void nhapfile(SV a[], int &n)
+void ghiFile(SV *a, int &n)
 {
 	FILE*f;
-	fopen_s(&f, "dssv.txt", "w+");
-	for (int i = 0; i < n; i++) {
-		fprintf(f, "%d\t%s\t%f\t%f\t%f\n", a[i].MSSV, a[i].hoten, a[i].toan, a[i].ly, a[i].hoa);
-	}
+	fopen_s(&f, "dssv.dat", "wb");
+	fwrite(a, sizeof(SV), n, f);
 	printf("Da save file");
 	fclose(f);
 }
-void nhapmang(SV a[], int &n)
+void docTuFile(SV *a, int &n) {
+	FILE *f;
+	n = 0;
+	fopen_s(&f, "dssv.dat", "rb");
+	if (f != NULL) {
+		while (!feof(f))
+			if (fread(a + n, sizeof(SV), 1, f))
+				n++;
+		fclose(f);
+		printf("Da doc file");
+	}
+	else {
+		printf("Khong the doc file");
+	}
+}
+void nhapmang(SV *a, int &n)
 {	
-		printf("nhap n:");
-		scanf_s("%d", &n);
+	delete a;
+	printf("Nhap n:\n");
+	scanf_s("%d", &n);
+	a = new SV[n];
 
     for (int i = 0; i<n; i++)
 	{
-		printf("nhap MSSV:"); scanf_s("%d", &a[i].MSSV);
-		printf("nhap hoten:"); fseek(stdin, 0, SEEK_END); gets_s(a[i].hoten);
+		printf("nhap MSSV:"); scanf_s("%d", &(a+i)->MSSV);
+		printf("nhap hoten:"); fseek(stdin, 0, SEEK_END); gets_s((a+i)->hoten);
 		printf("nhap diem toan:"); scanf_s("%f", &a[i].toan);
 		printf("nhap diem ly:"); scanf_s("%f", &a[i].ly);
 		printf("nhap diem hoa:"); scanf_s("%f", &a[i].hoa);
     }
 	
 }
-void xuatmang(SV a[], int n)
+void xuatmang(SV *a, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
-		printf("mssv: %d\n", a[i].MSSV);
+		printf("mssv: %d\n", (a+i)->MSSV);
 		printf("hoten: %s\n", a[i].hoten);
 		printf("diem toan : %f\n", a[i].toan);
 		printf(" diem ly: %f\n", a[i].ly);
 		printf(" diem hoa: %f\n", a[i].hoa);
 	}
 }
-void diemtb(SV a[], int n)
+void diemtb(SV *a, int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -82,16 +97,40 @@ void svrot(SV a[], int n)
 
 	}
 }
+int lonNhat(int a, int b) {
+	if (a > b)
+		return a;
+	return b;
+}
+int beNhat(int a, int b) {
+	if (a < b)
+		return a;
+	return b;
+}
+int TimGiaTri(int a[], int l, int(*SoSanh)(int, int)) {
+	int giaTri = a[0];
+	for (int i = 1; i < l; i++) {
+		giaTri = (*SoSanh)(giaTri, a[i]);
+	}
+	return giaTri;
+}
 void main()
 {
-	SV a[MAX];
-	int n;
-	nhapmang(a, n);
-	xuatmang(a, n);
-	diemtb(a, n);
-	tbmax(a, n);
-	svdau(a, n);
-	svrot(a, n);
-	nhapfile(a, n);
+
+	int a[] = { -4,7,10,20 };
+	printf("Gia tri lon nhat la %d\n", TimGiaTri(a, 4, lonNhat));
+	printf("Gia tri be nhat la %d", TimGiaTri(a, 4, beNhat));
+
+	int n = 0;
+	SV *dsSV = new SV[n];
+
+	//void nhapmang(SV *a, int &n)
+	nhapmang(dsSV, n);
+	xuatmang(dsSV, n);
+	diemtb(dsSV, n);
+	tbmax(dsSV, n);
+	svdau(dsSV, n);
+	svrot(dsSV, n);
+	ghiFile(dsSV, n);
 	_getch();
 }
