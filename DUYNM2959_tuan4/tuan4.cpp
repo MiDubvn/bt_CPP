1fed96f19d2f2a48017d9c5a85a179d78ed7d680
DUY Nguyen Minh <DUY.NM2959@SINHVIEN.HOASEN.EDU.VN>
2017-10-05 01:28:41 +0700

    A

--- DUYNM2959_tuan4/tuan4.cpp
+++ DUYNM2959_tuan4/tuan4.cpp
@@ -1,30 +1,49 @@
 #include<iostream>	
 #include<conio.h>	
 #include<string>
+using namespace std;
 
-void A(char*a)
+void A()
 {
-	int i;
-	int n = strlen(a);
-	for (i = 0; i < n; i++)
+	char **arr = new char *[100];
+	int i = 0;
+	do
 	{
-		if (a[i] == ' '); break;
-	}
-	if (i == n)
+		*(arr + i) = new char[40];
+		cin >> *(arr + i);
+		i++;
+	} while (cin.get() !='\n') ;
+	
+	cout << *(arr + 0) << endl;
+	for (int j = 1; j < i - 1; j++)
 	{
-		printf(" ho va ten cua banj chi co 1 tu");
-		return;
+		cout << *(arr + j) << " ";
 	}
-	for (int j = 0; j < i; i++)
-		printf("%s\n", a[i]);
-	for (int j = i + 1; j < n; j++)
-		printf("%s", a[j]);
-}
+	cout << *(arr + (i - 1)) << endl;
+	}
 
+void b()
+{
+	char **arr = new char *[100];
+	int i = 0;
+	do
+	{
+		*(arr + i) = new char[40];
+		cin >> *(arr + i);
+		i++;
+	} while (cin.get() != '\n');
+
+	cout << *(arr + 0) << endl;
+	for (int j = 1; j < i - 1; j++)
+	{
+		cout << *(arr + j) << " ";
+	}
+	cout << *(arr + (i - 1)) << endl;
+}
 void main()
 {
 	char c[40];
-	printf(" nhap chuoi:"); gets_s(c);
-	A(c);
+	printf(" nhap chuoi:");
+	A();
 	_getch();
 }
