cb7ded6d158409818070e6dd3cfaef5117b3856d
DUY Nguyen Minh <DUY.NM2959@SINHVIEN.HOASEN.EDU.VN>
2017-10-05 03:25:03 +0700

    fix 4

--- DUYNM2959_tuan4/tuan4.cpp
+++ DUYNM2959_tuan4/tuan4.cpp
@@ -3,6 +3,11 @@
 #include<string>
 using namespace std;
 
+struct KyTu
+{
+	char kytu;
+	int dem;
+};
 void strsplit(char **&arr, int &n)
 {
 	arr = new char *[100];
@@ -60,9 +65,32 @@ void D()
 	strsplit(arr, i);
 	for (int j = i-1; j > 0; j--)
 	{
-		cout << *(arr + j) ;
+		cout << *(arr + j)<<" " ;
 	}
 	cout << *(arr + 0) ;
+}
+void E()
+{
+	char **arr = NULL;
+	int i = 0;
+	strsplit(arr, i);
+	cout << *(arr + i-1);
+}
+void F()
+{
+	char **arr = NULL;
+	int i = 0;
+	strsplit(arr, i);
+	for (int j = 0; j < i; j++)
+		if (arr[j][0]<'A' || arr[j][0]>'Z')
+			arr[j][0] -= 32;
+	cout << *(arr + 0)  << " ";
+	for (int j = 1; j < i - 1; j++)
+	{
+		cout << *(arr + j) << " ";
+	}
+	cout << *(arr + (i - 1)) << endl;
+	
 }
 void bai1()
 {
@@ -72,10 +100,41 @@ void bai1()
 	for (int i = len - 1; i >= 0; i--)
 		cout << str[i];
 }
+void bai2()
+{
+	char str[100];
+	gets_s(str);
+	int len = strlen(str);
+	KyTu *list = new KyTu[len];
+	int dodai = 0;
+	for (int i = 0; i < len; i++)
+	{
+		bool isNew = true;
+		for (int j = 0; j < dodai; j++)
+		{
+			if (list[j].kytu == str[i])
+			{
+				isNew = false;
+				list[j].dem++;
+			}
+		}
+		if (isNew)
+		{
+			list[dodai].kytu = str[i];
+			list[dodai].dem = 1;
+			dodai++;
+		}
+	}
+	for (int i = 0; i < dodai; i++)
+	{
+		cout << list[i].kytu << ": " << list[i].dem << endl;
+	}
+		
+}
 void main()
 {
 	char c[40];
 	printf(" nhap chuoi:");
-	D();
+	bai2();
 	_getch();
 }
